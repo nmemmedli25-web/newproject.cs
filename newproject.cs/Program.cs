7bb4159147d7fc8cc399cde99cc645198a29c38a
User <User@DESKTOP-7UJ801C>
2026-09-23 00:32:14 +0400

    Add project files.

---
+++ newproject.cs/Program.cs
@@ -0,0 +1,87 @@
+﻿/*tapsirig 1
+ Console.WriteLine("adinizi daxil edin");
+String name=Console.ReadLine();
+Console.WriteLine("yasinizi daxil edin");
+int age = int.Parse(Console.ReadLine());
+Console.WriteLine("boyunuzu daxil edin");
+double height = double.Parse(Console.ReadLine());
+Console.WriteLine("Salam,menim adim  " + name + "dir " + "." + "Men " + age + " yasim var"+","+"boyum "+ height + "m-dir.");
+*/
+/*2-Ci tapsirig
+const double Pi = 3.14;
+
+Console.WriteLine("radiusu daxil edin");
+double radius = double.Parse(Console.ReadLine());
+double sahe = Pi * radius * radius;
+Console.WriteLine("dairenin sahesi " + sahe + "  dir.");
+*/
+
+/*3-cu tapsirig  
+ Console.WriteLine("Manat daxil edin");
+double manat = double.Parse(Console.ReadLine());
+const double USD = 1.7;
+const double EUR=1.82;
+double dollar = manat / USD;
+double euro = manat / EUR;
+Console.WriteLine(dollar);
+Console.WriteLine(euro);
+*/
+/*
+Console.WriteLine(" fennin qiymetini daxil edin");
+int first = int.Parse(Console.ReadLine());
+Console.WriteLine(" fennin qiymetini edin");
+int second = int.Parse(Console.ReadLine());
+Console.WriteLine(" fennin qiymetinil edin");
+int third = int.Parse(Console.ReadLine());
+Console.WriteLine(" fennin qiymetini edin");
+int fourth = int.Parse(Console.ReadLine());
+Console.WriteLine(" fennin qiymetini edin");
+int fifth = int.Parse(Console.ReadLine());
+
+double ortaBal = (first + second + third + fourth + fifth) / 5;
+Console.WriteLine("orta bal"+ ortaBal);
+if (ortaBal < 51)
+{
+    Console.WriteLine("kesildiniz");
+
+}
+else if (ortaBal < 91){
+    Console.WriteLine("orta netice");
+
+
+}else
+{
+    Console.WriteLine("ela netice");
+}
+*/
+
+/*Console.WriteLine("Ilkin meblegi daxil edin:");
+double mebleg = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Nece il saxlayacaqsiniz:");
+int il = int.Parse(Console.ReadLine());
+
+const double faiz = 0.12;
+
+double gelecekMebleg = mebleg * (1 + faiz * il);
+
+Console.WriteLine("Gelecek mebleg: " + gelecekMebleg);
+*/
+/*
+Console.WriteLine("Mesafeni daxil edin:");
+double mesafe = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Serf olunan yanacagi daxil edin:");
+double yanacaq = double.Parse(Console.ReadLine());
+
+if (mesafe < 0 || yanacaq < 0)
+{
+    Console.WriteLine("Daxil edilen melumat yanlisdir!");
+}
+else
+{
+    double serfiyyat = (yanacaq / mesafe) * 100;
+
+    Console.WriteLine("100 km ucun serfiyyat: " + serfiyyat);
+}
+*/
